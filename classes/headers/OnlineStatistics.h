dd87b700ef301c0256fee8011d6066ec7654dd1f
Vasya <vasya.shilin.smol@gmail.com>
2026-05-30 21:52:33 +0300

    Изменения: 1)изменена папочная структура

--- classes/headers/OnlineStatistics.h
+++
@@ -1,7 +0,0 @@
-#ifndef FOURTHLAB_ONLINE_STATISTICS_H
-#define FOURTHLAB_ONLINE_STATISTICS_H
-
-// The online-statistics thematic task is intentionally not implemented yet.
-// This laboratory step keeps only the required LazySequence and Stream core.
-
-#endif
